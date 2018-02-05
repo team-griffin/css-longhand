4e4f24815d49bc5fd6d78ee096a5438c0570e952
Jack Ellis <jack.ellis@godaddy.com>
2018-02-05 14:46:47 +0000

    feat: upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import upperFirst from 'lodash.upperfirst';
-import reduce from 'lodash.reduce';
+import * as r from 'ramda';
+
+const upperFirst = (str) => str.charAt(0).toUpperCase() + str.substr(1);
+// const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
 
 const longhand = (coreProp, prop) => {
-  return reduce(prop, (result, value, key) => {
+  return r.reduce((result, key) => {
+    const k = `${coreProp}${upperFirst(key)}`;
+    const v = prop[key];
+
+    // if (isObject(v)) {
+    //   return {
+    //     ...result,
+    //     ...longhand(k, v),
+    //   }
+    // } else {
     return {
       ...result,
-      [`${coreProp}${upperFirst(key)}`]: value,
+      [k]: v,
     };
-  }, {
-  });
+    // }
+  }, {}, r.keys(prop));
 };
 
 export default longhand;
