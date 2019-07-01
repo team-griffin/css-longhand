f3a94cfccf7a950acf2f9ec075cc027f9894269f
Jack Ellis <jack.ellis@godaddy.com>
2019-07-01 16:17:59 +0100

    fix: remove ramda babel plugin

--- rollup/config.js
+++ rollup/config.js
@@ -1,29 +1,24 @@
 import babel from 'rollup-plugin-babel';
 import localResolve from 'rollup-plugin-local-resolve';
-import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/es/css-longhand.production.js',
+      file: 'dist/es/css-longhand.js',
       format: 'es',
     },
     {
-      file: 'dist/cjs/css-longhand.production.js',
+      file: 'dist/cjs/css-longhand.js',
       format: 'cjs',
     },
   ],
   plugins: [
     localResolve(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
     }),
-    uglify(),
   ],
+  external: [ 'ramda' ],
 };
