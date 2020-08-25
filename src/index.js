7d22bf34eea2af5340730533cf51c376fcb615ce
thd <tharisondenby@godaddy.com>
2020-08-25 16:04:49 +0100

    feat: remove ramda

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
-import * as r from 'ramda';
-
 const upperFirst = (str) => str.charAt(0).toUpperCase() + str.substr(1);
-// eslint-disable-next-line
-// const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
 
 const longhand = (coreProp, prop) => {
-  return r.reduce((result, key) => {
+  return Object.keys(prop).reduce((result, key) => {
     const k = `${coreProp}${upperFirst(key)}`;
     const v = prop[key];
 
-    // if (isObject(v)) {
-    //   return {
-    //     ...result,
-    //     ...longhand(k, v),
-    //   }
-    // } else {
     return {
       ...result,
       [k]: v,
     };
-    // }
-  }, {
-
-  }, r.keys(prop));
+  }, {});
 };
 
 export default longhand;
